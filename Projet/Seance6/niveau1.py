0894254c97053ec5d3f2b1a46f5f03140fc6eebe
Emmanuel <emmanuel.fantino.civ@gmail.com>
2026-01-22 13:22:10 +0100

    Add files via upload

---
+++ Projet/Seance6/niveau1.py
@@ -0,0 +1,10 @@
+import pygame
+
+def get_plateformes():
+    return [
+        pygame.Rect(200, 500, 300, 30),
+        pygame.Rect(600, 400, 250, 30),
+        pygame.Rect(1000, 300, 200, 30),
+        pygame.Rect(1400, 500, 300, 30),
+        pygame.Rect(1800, 400, 250, 30),
+    ]
