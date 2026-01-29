cfd68db5c7c645a4b668ec81602089830b3003f2
SEB <salutcavahein@gmail.com>
2026-01-29 14:13:29 +0100

    Add files via upload

---
+++ Projet/Projet/niveau1.py
@@ -0,0 +1,10 @@
+import pygame
+
+def get_plateformes():
+    return [
+        pygame.Rect(200, 6000, 308, 75),
+        pygame.Rect(400, 5800, 250, 30),
+        pygame.Rect(600, 5600, 200, 30),
+        pygame.Rect(1400, 500, 300, 30),
+        pygame.Rect(1800, 400, 250, 30),
+    ]
