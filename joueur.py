20c31169c915ceb55273512ca400ebdb98d0b511
SEB <salutcavahein@gmail.com>
2026-04-10 09:34:34 +0200

    Update joueur.py

--- joueur.py
+++ joueur.py
@@ -31,7 +31,7 @@ class Joueur(pygame.sprite.Sprite):
         #vvv Enlever le commentaire pour le mettre à côté de la porte enfer
         #self.rect = pygame.Rect(285, 840, 50, 100)
         #vvv Enlever le commentaire pour le mettre à côté de Caronte
-        #self.rect = pygame.Rect(3850, 5380, 50, 100)
+        self.rect = pygame.Rect(3850, 5380, 50, 100)
 
         # Frame joueur normal
         self.sprites_normal = []
